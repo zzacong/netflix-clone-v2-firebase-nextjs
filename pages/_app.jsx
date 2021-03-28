950b2d8560e027333b3ed0cb6f84d11eb8e1e868
zzacong <ongzz100@gmail.com>
2021-03-28 16:04:09 +1100

    feat(Setup): project cleanup

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix | Nextjs</title>
+      </Head>
+    </div>
+  )
+}
